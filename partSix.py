6b110720dbefe80fd48c9d134f9f80d0e7db4c88
JG-Steenkamp <js2436@kent.ac.uk>
2024-10-11 10:11:16 +0000

    part 6 done

--- partSix.py
+++ partSix.py
@@ -1,1 +1,8 @@
-
+age = int (input(" how old are you?"))
+std = input(" student or non student")
+if age <12 and age >=65 and std == "n/a" :
+    print("£5 please")
+elif age >= 12 and age <65 and std == "student":
+    print("£8 please")
+elif age >= 12 and age <65 and std == "non-student":
+    print("£10 please")
