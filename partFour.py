0545aa2910e9c10cacdebecc2b6cf0a5c9c5df19
JG-Steenkamp <js2436@kent.ac.uk>
2024-10-11 09:46:33 +0000

    part 4 done

--- partFour.py
+++ partFour.py
@@ -1,1 +1,8 @@
-
+user = input("username:")
+pswrd = int(input("password:"))
+if user == "james":
+    print("Welcome James")
+elif pswrd == "1234" :
+    print("Correct password")
+else: 
+    print("You are banned")
