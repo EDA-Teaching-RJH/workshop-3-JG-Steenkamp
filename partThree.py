6fe55d177bab900099f2b9210a7c398f0acd7e1e
JG-Steenkamp <js2436@kent.ac.uk>
2024-10-11 09:36:39 +0000

    part 3 done

--- partThree.py
+++ partThree.py
@@ -1,1 +1,12 @@
-
+score = float(input("whats your score?"))
+if score >= 90 and score <= 100 :
+    print("A")
+elif score <= 89 and score >= 80 :
+    print("B")
+elif score <= 79 and  score >= 70 :
+    print("C")
+elif score <= 69 and  score >= 60 :
+    print("D")
+else:
+    print("Fail!")
+                    
