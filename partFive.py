eee30ba5fa3eb6cdc9ca4c8cdd424ffc71445a61
JG-Steenkamp <js2436@kent.ac.uk>
2024-10-11 09:56:32 +0000

    part 5 done

--- partFive.py
+++ partFive.py
@@ -1,1 +1,8 @@
+day = input( "whats the day of the week?")
+if day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday":
+    print("It's a week day.")
+elif day == "Friday":
+    print("It's almost the weekend!")
+elif day == "Saturday" or day == "Sunday":
+    print("It's the weekend!")
 
